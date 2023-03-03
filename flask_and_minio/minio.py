9d0f9ad3f0aebdf7eb366bd47592f396bcd4df43
heysaeid <heysaeid92@gmail.com>
2023-03-03 23:34:52 +0330

    Add get_url method to FlaskAndMinio

--- flask_and_minio/minio.py
+++ flask_and_minio/minio.py
@@ -5,20 +5,23 @@ from minio import Minio
 class FlaskAndMinio:
 
     def __init__(self, app: Flask = None) -> None:
+        self.domain = None
         self.client = None
         if app is not None:
             self.init_app(app)
 
     def init_app(self, app: Flask) -> None:
         app.config.setdefault('MINIO_ENDPOINT', 'localhost:9000')
+        app.config.setdefault('MINIO_LOAD_BALANCER_URL', app.config['MINIO_ENDPOINT'])
         app.config.setdefault('MINIO_ACCESS_KEY', 'minioadmin')
         app.config.setdefault('MINIO_SECRET_KEY', 'minioadmin')
         app.config.setdefault('MINIO_SECURE', True)
         app.config.setdefault('MINIO_REGION', None)
         app.config.setdefault('MINIO_HTTP_CLIENT', None)
 
+        self.domain = app.config['MINIO_LOAD_BALANCER_URL']
         self.client = Minio(
-            endpoint=app.config['MINIO_ENDPOINT'],
+            endpoint=self.domain,
             access_key=app.config['MINIO_ACCESS_KEY'],
             secret_key=app.config['MINIO_SECRET_KEY'],
             secure=app.config['MINIO_SECURE'],
@@ -30,3 +33,7 @@ class FlaskAndMinio:
 
     def get_client(self) -> Minio:
         return self.client
+    
+    def get_url(self, bucker_name: str, object_name: str, **kwargs) -> str:
+        url = self.client.presigned_get_object(bucker_name, object_name, **kwargs)
+        return self.domain + '/' + '/'.join(url.split('/')[3:])   
